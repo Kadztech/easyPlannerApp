5268c40f00f4c8b9950ac6733954cea45a0ad1be
chomba <chomba@bongohive.co.zm>
2022-01-31 16:39:28 +0200

    adding Auth screens

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { SafeAreaView, View, Text } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import LoginScreen from './src/Screens/Auth/Login/';
+import RegisterScreen from './src/Screens/Auth/Register/';
 
-export default function App() {
+const AuthStack = createStackNavigator();
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Easy planner App</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <NavigationContainer>
+      <AuthStack.Navigator>
+        <AuthStack.Screen name="Login" component={LoginScreen} />
+        <AuthStack.Screen name="Register" component={RegisterScreen} />
+      </AuthStack.Navigator>
+    </NavigationContainer>
+    // <SafeAreaView style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+    //   <Text>Easy planner v1.0</Text>
+    // </SafeAreaView>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
