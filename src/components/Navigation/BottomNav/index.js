359ecf91872ec3060dea67bccdbe91f5bc8cd678
chomba <chomba@bongohive.co.zm>
2022-01-31 16:47:53 +0200

    bottom navigation

---
+++ src/components/Navigation/BottomNav/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { SafeAreaView, Text } from 'react-native-safe-area-context';
+
+const BottomNav = () => {
+    return (
+        <SafeAreaView>
+            <Text>BottomNav</Text>
+        </SafeAreaView>
+    )
+}
+
+export default BottomNav;
