daa65e94412f5b9d3c053ceaa7823482c794b2ac
chomba <chomba@bongohive.co.zm>
2022-01-31 16:45:30 +0200

    initialise main screen

---
+++ src/Screens/Events/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { SafeAreaView, Text } from 'react-native-safe-area-context';
+
+const EventsScreen = () => {
+    return (
+        <SafeAreaView>
+            <Text>Events Screen</Text>
+        </SafeAreaView>
+    )
+}
+export default EventsScreen;
